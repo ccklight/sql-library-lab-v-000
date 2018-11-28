688c17ec05663bf589164ccd1bd0906092bd0cfc
Cheryl K <cherylkelson@icloud.com>
2018-11-28 23:10:26 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO series (id, title, autho_id, subgenre_id) VALUES (1, "Windswept Land of Narnia", 1, 1), (2, "Paradise Fantasyland", 2, 2);
+INSERT INTO sub-genres(id, name) VALUES (1 "romance"), (2, "historic");
+INSERT INTO authors (id, name) VALUES (1, "Zora Neal Hurston"), (2, "Tim Ferriss");
+INSERT INTO books (id, title, year, series_id) VALUES (1, "The Hobbitt", 1975, 3), (2, "Tolkien Rules Gandolph", 1978, 7), (3, "Mystic Magic", 1982,4);
